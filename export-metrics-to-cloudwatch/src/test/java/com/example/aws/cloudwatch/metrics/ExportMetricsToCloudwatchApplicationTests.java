fcbf7269d8efd6933ccb473f2941b74bc89baf90
Artem Areshko <artemaa@gmail.com>
2020-03-12 15:36:57 +0300

    Export metrics to cloudwatch demo

---
+++ export-metrics-to-cloudwatch/src/test/java/com/example/aws/cloudwatch/metrics/ExportMetricsToCloudwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws.cloudwatch.metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportMetricsToCloudwatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
